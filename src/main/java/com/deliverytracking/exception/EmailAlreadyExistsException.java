0473cbe59b7e702a5bf594ec5fae0d35230b67d1
Swatipatri21 <swati21shree@gmail.com>
2026-04-01 16:53:04 +0530

    Delete src directory

--- src/main/java/com/deliverytracking/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.deliverytracking.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
